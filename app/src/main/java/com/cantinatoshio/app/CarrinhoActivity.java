4b51eeb77a2c35e1eae6a50795711d91726b5607
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-11-07 22:08:37 -0300

    Fazendo carrinho

---
+++ app/src/main/java/com/cantinatoshio/app/CarrinhoActivity.java
@@ -0,0 +1,20 @@
+package com.cantinatoshio.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class CarrinhoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrinho);
+
+        ListView lista_carrinho;
+
+
+
+    }
+}

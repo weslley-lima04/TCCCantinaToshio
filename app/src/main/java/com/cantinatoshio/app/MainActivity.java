cb192e3ede724cf0fb0d89fc321501b36c7046a5
weslley-lima24 <weslley.lima24@etec.sp.gov.br>
2022-12-08 20:12:52 -0300

    Com menu do navDrawer funcionando

--- app/src/main/java/com/cantinatoshio/app/MainActivity.java
+++ app/src/main/java/com/cantinatoshio/app/MainActivity.java
@@ -2,23 +2,32 @@ package com.cantinatoshio.app;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cantinatoshio.app.api.Api;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import kotlin.time.TimeSource;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -28,6 +37,13 @@ public class MainActivity extends AppCompatActivity
     PedidosFragment pedidosFragment = new PedidosFragment();
     PerfilFragment perfilFragment = new PerfilFragment();
     FloatingActionButton btnCart;
+
+    //Drawer Layout
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    MaterialToolbar toolbar;
+    View headerview;
+    Menu menu;
 
 
     @Override
@@ -40,9 +56,39 @@ public class MainActivity extends AppCompatActivity
         //chamando lista pedidos
         new Cliente().getPedidosCliente();
 
-
-
         btnCart = findViewById(R.id.btnCart);
+
+        //ID NavDrawer
+        toolbar = findViewById(R.id.idToolBar);
+        drawerLayout = findViewById(R.id.idNavDrawer);
+        navigationView = findViewById(R.id.idNavigationView);
+
+                //getItem(R.id.menuPerfil_sair);
+
+        //acessando os itens do header
+        headerview = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerview.findViewById(R.id.navUserName);
+        TextView navUseremail = (TextView) headerview.findViewById(R.id.navUserEmail);
+        CircleImageView userImage = (CircleImageView) headerview.findViewById(R.id.circleImageView);
+        menu = navigationView.getMenu();
+        MenuItem exit = menu.findItem(R.id.nav_menu_exit);
+
+
+        //clique para abrir o NavDrawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //setando infos do perfil conforme login
+
+        if(!Cliente.isLoggedIn)
+        {
+            navUsername.setVisibility(View.GONE);
+            navUseremail.setVisibility(View.GONE);
+            userImage.setVisibility(View.GONE);
+            exit.setIcon(R.drawable.ic_baseline_arrow_back);
+            exit.setTitle("Entrar");
+        }
 
         //parte do bottom navigation
 
@@ -57,10 +103,10 @@ public class MainActivity extends AppCompatActivity
                 switch (item.getItemId())
                 {
                     case R.id.ic_Home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, homeFragment).commit();
+                        callFragment(homeFragment);
                         return true;
                     case R.id.ic_pedidos:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, pedidosFragment).commit();
+                        callFragment(pedidosFragment);
                         return true;
                     case R.id.ic_perfil:
                         getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, perfilFragment).commit();
@@ -87,5 +133,6 @@ public class MainActivity extends AppCompatActivity
     {
         getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, fragment).commit();
     }
+
 
 }

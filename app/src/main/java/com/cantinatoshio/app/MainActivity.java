fd3bc899b50e70759c4e620b5b425c3c1978ec70
weslley-lima24 <weslley.lima24@etec.sp.gov.br>
2022-12-10 10:56:22 -0300

    Click da nav funcionando

--- app/src/main/java/com/cantinatoshio/app/MainActivity.java
+++ app/src/main/java/com/cantinatoshio/app/MainActivity.java
@@ -2,41 +2,38 @@ package com.cantinatoshio.app;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.cantinatoshio.app.api.Api;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.tabs.TabLayout;
 
 import de.hdodenhof.circleimageview.CircleImageView;
-import kotlin.time.TimeSource;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     ActionBarDrawerToggle toggle;
     BottomNavigationView bottomNavigationView;
     HomeFragment homeFragment = new HomeFragment();
     PedidosFragment pedidosFragment = new PedidosFragment();
-    PerfilFragment perfilFragment = new PerfilFragment();
+  //  PerfilFragment perfilFragment = new PerfilFragment();
     FloatingActionButton btnCart;
 
     //Drawer Layout
@@ -66,9 +63,14 @@ public class MainActivity extends AppCompatActivity
 
         //ID NavDrawer
         toolbar = findViewById(R.id.idToolBar);
-        drawerLayout = findViewById(R.id.idNavDrawer);
+        drawerLayout = findViewById(R.id.idDrawerLayout);
         navigationView = findViewById(R.id.idNavigationView);
+        navigationView.bringToFront();
 
+        navigationView.setNavigationItemSelectedListener(this);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         //acessando os itens do header
         headerview = navigationView.getHeaderView(0);
@@ -80,9 +82,7 @@ public class MainActivity extends AppCompatActivity
 
 
         //clique para abrir o NavDrawer
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+
 
         //setando infos do perfil conforme login
 
@@ -95,22 +95,6 @@ public class MainActivity extends AppCompatActivity
             exit.setTitle("Entrar");
         }
 
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
-        {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item)
-            {
-                System.out.println("INDO PRO CLICK NAVIGATION");
-                if (item.getItemId() == R.id.nav_menu_exit)
-                {
-                        Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), LogarActivity.class);
-                        startActivity(intent);
-                }
-                return true;
-            }
-        });
 
 
 
@@ -158,4 +142,49 @@ public class MainActivity extends AppCompatActivity
         getSupportFragmentManager().beginTransaction().replace(R.id.FrameContainer, fragment).commit();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+
+        System.out.println("INDO PRO CLICK NAVIGATION");
+        if (item.getItemId() == R.id.nav_menu_exit)
+        {
+            Toast.makeText(MainActivity.this, "Click", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(getApplicationContext(), LogarActivity.class);
+            startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState)
+    {
+        super.onPostCreate(savedInstanceState, persistentState);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
 }

a7b75eaa47918b1729d601ae0f4a2911a8fd6fe7
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-12-12 21:30:08 -0300

    Ajustes no layout

--- app/src/main/java/com/cantinatoshio/app/api/Api.java
+++ app/src/main/java/com/cantinatoshio/app/api/Api.java
@@ -2,7 +2,7 @@ package com.cantinatoshio.app.api;
 
 public class Api
 {
-    private static final String ROOT_URL = "http://192.168.1.14/CantinaAPI/CantinaAPI/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.56.1/CantinaAPI/CantinaAPI/v1/Api.php?apicall=";
 
     public static final String URL_CREATE_HERO = ROOT_URL + "createProdutos";
     public static final String URL_READ_HEROES = ROOT_URL + "getProdutos";
@@ -14,7 +14,7 @@ public class Api
 
     public static final String URL_CLIENTE_PEDIDOS = ROOT_URL + "getClientePedidos";
 
-    public static final String URL_LAST_IDPEDIDO = "http://192.168.1.14/CantinaApi/CantinaAPI/includes/getPedidoID.php";
+    public static final String URL_LAST_IDPEDIDO = "http://192.168.56.1/CantinaApi/CantinaAPI/includes/getPedidoID.php";
 
 
     public static final String URL_LOGAR = ROOT_URL + "logar";
